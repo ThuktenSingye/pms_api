86ea11ee685ddb0e5cf84e2b9ef1a6cae14a2845
ThuktenSingye <thuktensingye2163@gmail.com>
2023-11-13 00:48:38 +0600

    server log

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.use('/projects', projectRouter);
 
 
 app.listen(process.env.APP_PORT, ()=>{
-    console.log('Server up and running on port:',process.env.APP_PORT);
+    console.log("Connected Succesfully");
 })
